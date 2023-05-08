0751c4b68a7c0c5f565973b93fff30c09846a27d
KoenPasteels <kpasteels@hotmail.com>
2023-05-08 18:37:42 +0200

    Adaptive color red circles

---
+++ src/View/Converters/SquareStatusConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace View.Converters
+{
+    class SquareStatusConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Brushes.Red;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
