1415d333ee059d5af44e239f4ce874846503c87f
KoenPasteels <kpasteels@hotmail.com>
2023-05-09 18:50:03 +0200

    Uncovering squares

--- src/ViewModel/SquareViewModel.cs
+++ src/ViewModel/SquareViewModel.cs
@@ -2,16 +2,19 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace ViewModel
 {
     public class SquareViewModel
     {
         public Square Square { get; }
+        public ICommand Uncover { get; }
 
         public SquareViewModel(Square square)
         {
             this.Square = square;
+            this.Uncover = new UncoverSquareCommand();
         }
     }
 }
---
+++ src/ViewModel/UncoverSquareCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows.Input;
+
+namespace ViewModel
+{
+    class UncoverSquareCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            Debug.WriteLine("You clicked me");
+        }
+    }
+}
