172fb8af3ecf22773549e4399a8e6efb88894d29
KoenPasteels <kpasteels@hotmail.com>
2023-05-11 19:07:54 +0200

    StartScreen

---
+++ src/ViewModel/StartScreenViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Cells;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace ViewModel
+{
+    public class StartScreenViewModel : ScreenViewModel
+    {
+        public StartScreenViewModel(ICell<ScreenViewModel> currentScreen) : base(currentScreen)
+        {
+            SwitchToPlayScreen = new ActionCommand(() => CurrentScreen.Value = new PlayScreenViewModel(this.CurrentScreen));
+
+            SwitchToSettingsScreen = new ActionCommand(() => CurrentScreen.Value = new SettingsScreenViewModel(this.CurrentScreen));
+        }
+
+        public ICommand SwitchToPlayScreen { get; }
+
+        public ICommand SwitchToSettingsScreen { get; }
+    }
+}
