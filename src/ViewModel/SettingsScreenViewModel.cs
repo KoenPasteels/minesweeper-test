22f8b280dab9a1a3edb3440e033ce9fbf6500a2c
KoenPasteels <kpasteels@hotmail.com>
2023-05-11 19:08:08 +0200

    SettingsScreen

---
+++ src/ViewModel/SettingsScreenViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Cells;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace ViewModel
+{
+    public class SettingsScreenViewModel : ScreenViewModel
+    {
+        public SettingsScreenViewModel(ICell<ScreenViewModel> currentScreen) : base(currentScreen)
+        {
+            SwitchToStartScreen = new ActionCommand(() => CurrentScreen.Value = new StartScreenViewModel(this.CurrentScreen));
+            SwitchToPlayScreen = new ActionCommand(() => CurrentScreen.Value = new PlayScreenViewModel(this.CurrentScreen));
+        }
+
+        public ICommand SwitchToStartScreen { get; }
+
+        public ICommand SwitchToPlayScreen { get; }
+    }
+}
