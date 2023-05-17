fee070ebd6f7132d2e30b1a27469521634953296
KoenPasteels <kpasteels@hotmail.com>
2023-05-17 19:40:35 +0200

    No more startscreen

--- src/ViewModel/MainViewModel.cs
+++ src/ViewModel/MainViewModel.cs
@@ -13,7 +13,7 @@ namespace ViewModel
             CurrentScreen = Cell.Create<ScreenViewModel>(null);
 
             // Create start screen
-            var firstScreen = new StartScreenViewModel(CurrentScreen);
+            var firstScreen = new SettingsScreenViewModel(CurrentScreen);
 
             // Put first screen in CurrentScreen cell
             CurrentScreen.Value = firstScreen;
