dca049558e0472c7c0b9b96d82a54ead741a586c
KoenPasteels <kpasteels@hotmail.com>
2023-05-22 21:23:50 +0200

    Set up settingsScreen

--- src/ViewModel/SettingsScreenViewModel.cs
+++ src/ViewModel/SettingsScreenViewModel.cs
@@ -11,17 +11,26 @@ namespace ViewModel
     {
         public SettingsScreenViewModel(ICell<ScreenViewModel> currentScreen) : base(currentScreen)
         {
-            //SwitchToPlayScreen = new ActionCommand(() => CurrentScreen.Value = new PlayScreenViewModel(this.CurrentScreen, boardsize, mineprobability, flooding));
-            
+            SwitchToPlayScreen = new ActionCommand(() => CurrentScreen.Value = new GameViewModel(IGame.CreateRandom(BoardSize.Value, 0.2, Flooding), CurrentScreen));
+
+            this.BoardSize = Cell.Create(5);
             this.MinimumBoardsize = IGame.MinimumBoardSize;
             this.MaximumBoardsize = IGame.MaximumBoardSize;
-            //this.MineProbability = ;
+            //this.MineProbability = 0.2;
+
+            Start5 = new ActionCommand(() => CurrentScreen.Value = new GameViewModel(IGame.CreateRandom(5, 0.2, true), CurrentScreen));
+            Start10 = new ActionCommand(() => CurrentScreen.Value = new GameViewModel(IGame.CreateRandom(10, 0.2, true), CurrentScreen));
+            Start20 = new ActionCommand(() => CurrentScreen.Value = new GameViewModel(IGame.CreateRandom(20, 0.2, true), CurrentScreen));
         }
 
         public ICommand SwitchToPlayScreen { get; }
+        public ICommand Start5 { get; }
+        public ICommand Start10 { get; }
+        public ICommand Start20 { get; }
 
         public int MinimumBoardsize { get; }
         public int MaximumBoardsize { get; }
+        public ICell<int> BoardSize { get; set; }
         public bool Flooding { get; set; }
         //public int MineProbability { get; }
     }
