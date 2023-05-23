9835d545f5c16118986ad2b9cc049807fe97a508
KoenPasteels <kpasteels@hotmail.com>
2023-05-23 19:10:46 +0200

    Trying to get starNewGame running

--- src/ViewModel/GameViewModel.cs
+++ src/ViewModel/GameViewModel.cs
@@ -3,6 +3,7 @@ using Model.Data;
 using Model.MineSweeper;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 using System.Windows.Input;
 
@@ -12,17 +13,22 @@ namespace ViewModel
     {
         protected ICell<IGame> game;
         public ICell<GameBoardViewModel> Board { get; }
+        //public int BoardSize { get; set; }
         //public bool Flooding { get; }
 
         public GameViewModel(IGame igame, ICell<ScreenViewModel> currentScreen) : base(currentScreen)
         {
             this.game = Cell.Create(igame);
             this.Board = game.Derive(g => new GameBoardViewModel(game));
-            var boardSize = Board.Value.Height;
+            //this.BoardSize = Board.Value.Height;
 
             SwitchToSettingsScreen = new ActionCommand(() => CurrentScreen.Value = new SettingsScreenViewModel(this.CurrentScreen));
+            //Debug.WriteLine("YOOO The var is: " + Board.Value.Height);
+            //Debug.WriteLine("YOOO The size is: " + BoardSize);
+            //StartNewGame = new ActionCommand(() => CurrentScreen.Value = new GameViewModel(IGame.CreateRandom(Board.Value.Height, 0.2, Flooding), this.CurrentScreen));
         }
 
         public ICommand SwitchToSettingsScreen { get; }
+        public ICommand StartNewGame { get; }
     }
 }
