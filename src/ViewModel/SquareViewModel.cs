940fbec61536173e90ea8986f0a6600c40a4325b
KoenPasteels <kpasteels@hotmail.com>
2023-05-23 21:13:03 +0200

    Show bombs at end of game

--- src/ViewModel/SquareViewModel.cs
+++ src/ViewModel/SquareViewModel.cs
@@ -11,12 +11,28 @@ namespace ViewModel
     public class SquareViewModel
     {
         public ICell<Square> Square{ get; }
+        public ICell<IGame> Game { get; }
+        public ICell<bool> ContainsMine { get; }
         public ICommand Uncover { get; }
         public ICommand Flag { get; }
 
         public SquareViewModel(ICell<IGame> game, Vector2D position)
         {
             this.Square = game.Derive(g => g.Board[position]);
+            this.Game = game;
+            this.ContainsMine = game.Derive(g =>
+                {
+                    if (game.Value.Status != GameStatus.InProgress)
+                    {
+                        if (g.Mines.Contains(position))
+                        {
+                            return true;
+                        }
+                        else return false;
+                    }
+                    else return false;
+                });
+
             this.Uncover = new UncoverSquareCommand(game, position);
             this.Flag = new FlagSquareCommand(game, position);
         }
