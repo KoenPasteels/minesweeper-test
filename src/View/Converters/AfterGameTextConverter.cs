9106ef1997f59d73914ba059d23244867ba8402d
KoenPasteels <kpasteels@hotmail.com>
2023-05-23 22:02:05 +0200

    Starting on text converter for gameStatus

---
+++ src/View/Converters/AfterGameTextConverter.cs
@@ -0,0 +1,37 @@
+﻿using Model.MineSweeper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace View.Converters
+{
+    class AfterGameTextConverter : IValueConverter
+    {
+        public object InProgress { get; }
+        public object Won { get; }
+        public object Lost { get; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var text = (GameStatus)value;
+            switch (text)
+            {
+                case GameStatus.InProgress:
+                    return InProgress;
+                case GameStatus.Won:
+                    return Won;
+                case GameStatus.Lost:
+                    return Lost;
+                default:
+                    throw new InvalidCastException();
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
