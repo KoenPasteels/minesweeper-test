d4975e401a7ca8f7cdd1de5fb724a5bbafb52d47
KoenPasteels <kpasteels@hotmail.com>
2023-05-24 19:03:37 +0200

    Custom mine probability and text labels for sliders

--- src/ViewModel/SettingsScreenViewModel.cs
+++ src/ViewModel/SettingsScreenViewModel.cs
@@ -11,12 +11,14 @@ namespace ViewModel
     {
         public SettingsScreenViewModel(ICell<ScreenViewModel> currentScreen) : base(currentScreen)
         {
-            SwitchToPlayScreen = new ActionCommand(() => CurrentScreen.Value = new GameViewModel(IGame.CreateRandom(BoardSize.Value, 0.2, Flooding), CurrentScreen));
+            SwitchToPlayScreen = new ActionCommand(() => CurrentScreen.Value = new GameViewModel(IGame.CreateRandom(BoardSize.Value, MineProbability.Value, Flooding), CurrentScreen));
 
             this.BoardSize = Cell.Create(5);
+            this.MineProbability = Cell.Create(0.1);
             this.MinimumBoardsize = IGame.MinimumBoardSize;
             this.MaximumBoardsize = IGame.MaximumBoardSize;
-            //this.MineProbability = 0.2;
+            this.MinimumMineProbability = 0.1;
+            this.MaximumMineProbability = 0.5;
 
             Start5 = new ActionCommand(() => CurrentScreen.Value = new GameViewModel(IGame.CreateRandom(5, 0.2, true), CurrentScreen));
             Start10 = new ActionCommand(() => CurrentScreen.Value = new GameViewModel(IGame.CreateRandom(10, 0.2, true), CurrentScreen));
@@ -32,6 +34,8 @@ namespace ViewModel
         public int MaximumBoardsize { get; }
         public ICell<int> BoardSize { get; set; }
         public bool Flooding { get; set; }
-        //public int MineProbability { get; }
+        public double MinimumMineProbability { get; }
+        public double MaximumMineProbability { get; }
+        public ICell<double> MineProbability { get; set; }
     }
 }
